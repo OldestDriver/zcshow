f454ce766d545daaed8ab5cee01975c52392f9ad
xuningxin <xu509@outlook.com>
2019-07-17 16:30:30 +0800

    123

---
+++ Assets/TestCameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //      连接卡菲
+
+        
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 读取视频流
+
+        // 解码视频流并显示
+    }
+}
