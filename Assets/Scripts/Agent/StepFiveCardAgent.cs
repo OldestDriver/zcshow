89bc1cd758119b1f571c5d6c6af37a1b08896a9f
zhangxing <873074332@qq.com>
2019-07-18 09:33:18 +0800

    1

--- Assets/Scripts/Agent/StepFiveCardAgent.cs
+++ Assets/Scripts/Agent/StepFiveCardAgent.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.Video;
+using BestHTTP;
+using System;
+using LitJson;
+using System.IO;
 
 public class StepFiveCardAgent : CardBaseAgent
 {
@@ -12,12 +16,21 @@ public class StepFiveCardAgent : CardBaseAgent
     [SerializeField] private RectTransform _loadingContentRect;
     [SerializeField] private RectTransform _loadingRect;
     [SerializeField] private RectTransform _resultRect;
+    [SerializeField] private RawImage _qrCode;
 
 
     private bool _erCodeIsGenerated = false;
     private bool _showResultLock = false;
     private bool _sendingEmail = false;
 
+    //公共参数
+    private int APP_ID = 99;
+    private string api = "https://api.pixelplus.cc";
+    private string qiniu_api = "https://upload.qiniu.com";
+    private string h5_api = "https://h5.pixelplus.cc";
+    private int EVENT_ID = 49;
+
+    private string message_id;
 
     private void Reset()
     {
@@ -110,9 +123,17 @@ public class StepFiveCardAgent : CardBaseAgent
     }
 
 
-    private void ShowResult() {
+    private void ShowResult()
+    {
         _resultRect.gameObject.SetActive(true);
         _loadingRect.gameObject.SetActive(false);
+    }
+
+
+    //流程出错
+    void Error()
+    {
+        Debug.Log("流程内出错了！！！");
     }
 
 
@@ -122,10 +143,112 @@ public class StepFiveCardAgent : CardBaseAgent
     private void GetErCode() {
 
         // 上传视频功能
+        //获取token
+        GetToken();
+
 
         // 小程序码获取功能
-        StartCoroutine(DoMockGenerate());
+        //StartCoroutine(DoMockGenerate());
 
+    }
+
+    void GetToken()
+    {
+        HTTPRequest request = new HTTPRequest(new Uri(api + "/api/storage/qiniu/token"), HTTPMethods.Post, GetTokenRequestFinished);
+        request.AddField("strategy", "default");
+        request.AddField("app", "1");
+        request.Send();
+    }
+
+    void GetTokenRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        JsonData data = JsonMapper.ToObject(response.DataAsText);
+        if ((bool)data["status"])
+        {
+            //获取token请求成功
+            string token = (string)data["data"]["token"];
+            //上传视频
+            UploadVideo(token);
+        }
+        else
+        {
+            Debug.Log("请求token失败");
+            Error();
+        }
+    }
+
+    void UploadVideo(string token)
+    {
+
+        HTTPRequest request = new HTTPRequest(new Uri(qiniu_api), HTTPMethods.Post, UploadVideoRequestFinished);
+        request.AddBinaryData("file", GetVideoData(Application.dataPath + "/Files/1.mp4"));
+        request.AddField("token", token);
+        request.Send();
+    }
+
+    byte[] GetVideoData(string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        byte[] data = new byte[fs.Length];
+        fs.Read(data, 0, data.Length);
+        fs.Close();
+        return data;
+    }
+
+    void UploadVideoRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        JsonData data = JsonMapper.ToObject(response.DataAsText);
+        if ((bool)data["status"])
+        {
+            //上传视频成功
+            string videoUrl = (string)data["data"]["url"];
+            UploadUrl(videoUrl);
+            Debug.Log("上传视频成功:" + videoUrl);
+        }
+        else
+        {
+            Debug.Log("上传视频失败");
+            Error();
+        }
+    }
+
+    void UploadUrl(string url)
+    {
+        HTTPRequest request = new HTTPRequest(new Uri(api+"/api/attachments"), HTTPMethods.Post, UploadUrlRequestFinished);
+        request.AddField("app_id", APP_ID.ToString());
+        request.AddField("file_url", url);
+        request.Send();
+    }
+
+    void UploadUrlRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        JsonData data = JsonMapper.ToObject(response.DataAsText);
+        if ((bool)data["status"])
+        {
+            //上传文件路径成功
+            Debug.Log(response.DataAsText);
+            return;
+            message_id = (string)data["data"]["options"]["mail"]["message"];
+            string code = (string)data["data"]["code"];
+            GetQRCode(code);
+        }
+        else
+        {
+            Debug.Log("上传文件路径成功");
+            Error();
+        }
+    }
+
+    void GetQRCode(string code)
+    {
+        HTTPRequest request = new HTTPRequest(new Uri(h5_api + "/api/wxapp/getFileQrcode/" + code + "?type=beat_2"), HTTPMethods.Get, GetQRCodeRequestFinished);
+        request.Send();
+    }
+
+    void GetQRCodeRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        _qrCode.texture = response.DataAsTexture2D;
+        _erCodeIsGenerated = true;
     }
 
     IEnumerator DoMockGenerate()
